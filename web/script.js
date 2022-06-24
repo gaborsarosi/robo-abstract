2192b2d36d2a7fe7f8139df829e1817ef581c9c1
gaborsarosi <107033067+gaborsarosi@users.noreply.github.com>
2022-06-24 14:34:34 +0200

    Update script.js

--- web/script.js
+++ web/script.js
@@ -1,27 +1,46 @@
 
+//Position of real and fake abstracts. 1=left, 2=right
+let realnum = 2;
+let fakenum = 1;
+
+//Load data from JSON file
+let data;    
+fetch("data.json").then(
+        function(u){ return u.json();}
+      ).then(
+        function(json){
+          data = json;
+        }
+      ).then(()=>{setTitle();})
+
+//fetch("data.json").then(response => response.json()).then(data => {console.log(data)})
+
 //Function giving a random integer 0,1,...,max-1
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
 
-//Position of real and fake abstracts. 1=left, 2=right
-let realnum = 2;
-let fakenum = 1;
 
 //Loads a random entry from data.json. Selects a random position for the real abstract.
 function setTitle() {
+    //document.getElementById("articletitle").textContent = 'a title' + realnum;
     realnum = getRandomInt(2)+1;
     fakenum = (realnum )%2 +1;
     m = getRandomInt(data.length);
-    document.getElementById("articletitle").textContent = 'Title: '+data[m]['title'];
+    document.getElementById("articletitle").textContent = data[m]['title'];
     document.getElementById("button"+realnum).textContent = data[m]['real_abstract'];
     document.getElementById("button"+fakenum).textContent = data[m]['fake_abstract'];
 }
 
+
+
 //Your total tries and correct guesses
 let tries = 0;
 let correct = 0;
+
+//Initialization
+//setTitle();
 
 //Called when an abstract is selected. Argument n=1: left, n=2: right is selected. Determines if the selection was correct.
 function buttonClicked(n) {
@@ -31,6 +50,6 @@ function buttonClicked(n) {
     } else {
         tries+= 1;
     }
-    document.getElementById("result").textContent = correct + ' correct out of ' + tries + ' guesses (' + 100*correct/tries + '% percent)';
+    document.getElementById("result").textContent = correct + ' correct out of ' + tries + ' guesses (' + 100*correct/tries + '% accuracy)';
     setTitle();
 }
